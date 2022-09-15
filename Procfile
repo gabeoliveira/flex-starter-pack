aec90937100fa68a1520e3942f349af7b7e3d564
Gabriel Oliveira <goliveira@twilio.com>
2022-09-15 12:05:22 -0300

    Updated Procfile

---
+++ Procfile
@@ -0,0 +1,3 @@
+web: npm install -g twilio-cli@latest
+web: twilio plugins:install @twilio-labs/plugin-flex
+web: node ./server/bin/www
--- server/app.js
+++ server/app.js
@@ -4,8 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const port = 3001;
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const deployRouter = require('./routes/deploy');
