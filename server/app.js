c2c9b0cf20cc121e37a3f9debd00554abe1e160e
Gabriel Oliveira <gabriel.ilv@gmail.com>
2022-10-04 16:33:27 -0300

    Client app build

--- server/app.js
+++ server/app.js
@@ -4,11 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 const deployRouter = require('./routes/deploy');
 
-var app = express();
+const publicPath = path.join(__dirname, '..', 'client', 'starter-pack-client', 'build');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,16 +18,19 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(publicPath));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/deploy', deployRouter);
+
+app.get('/', function(req, res, next) {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 // catch 4t04 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
